359b746bfd3e44c913812692968968006056ff84
berksavcioglu <93647142+berksavcioglu@users.noreply.github.com>
2022-12-01 21:52:57 +0300

    network
    header kısmının componentlerini oluşturuyorum

--- ilkproje/pages/index.vue
+++ ilkproje/pages/index.vue
@@ -1,14 +1,29 @@
 <template>
              
-<h1>cemsavci</h1>
+<app-header/>
 
 
 </template>
 
 <script>
+import AppHeader from '../components/AppHeader.vue';
 
- export default {
+
+
+export default {
+  components: { AppHeader ,},
   name: 'IndexPage'}
   
+
  
 </script>
+<style>
+#index {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
